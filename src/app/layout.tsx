2ca6c9abfb6b346cdf2b320b947019093cc36426
Areeb ur Rub <areeburrub@gmail.com>
2024-07-26 03:00:22 +0530

    add: Vercel Speed Insight

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 import type {Metadata} from "next";
 import {Inter} from "next/font/google";
@@ -39,6 +40,7 @@ export default function RootLayout({
         <html lang="en">
         <body className={inter.className}>
             <Analytics/>
+            <SpeedInsights/>
             <main>{children}</main>
             <Toaster/>
         </body>
